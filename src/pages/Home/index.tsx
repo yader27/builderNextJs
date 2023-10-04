fc620f3cfd6700f66e6928e5f7ec1fb78673fe4a
YDR27 <yosuna@istmocenter.com>
2023-10-04 14:31:47 -0600

    add initial proyect

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+
+export default function Home() {
+  return (
+    <section className="bg-stone-900 z-[1] flex max-w-full w-[1696px] flex-col p-5 rounded-[100px]">
+      <div className="flex max-w-full w-[1567px] flex-row items-start self-center justify-between gap-5 -ml-1 mt-px -mb-px max-md:flex-wrap max-md:mb-2.5">
+        <section className="flex max-w-full w-[793px] flex-row items-start justify-between gap-5 max-md:flex-wrap">
+          <img loading="lazy" srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/9add3222-3b43-491c-aca9-d64cf4be8531?apiKey=4638d3c0f2964543b1679eba321e1d84&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/9add3222-3b43-491c-aca9-d64cf4be8531?apiKey=4638d3c0f2964543b1679eba321e1d84&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/9add3222-3b43-491c-aca9-d64cf4be8531?apiKey=4638d3c0f2964543b1679eba321e1d84&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/9add3222-3b43-491c-aca9-d64cf4be8531?apiKey=4638d3c0f2964543b1679eba321e1d84&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/9add3222-3b43-491c-aca9-d64cf4be8531?apiKey=4638d3c0f2964543b1679eba321e1d84&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/9add3222-3b43-491c-aca9-d64cf4be8531?apiKey=4638d3c0f2964543b1679eba321e1d84&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/9add3222-3b43-491c-aca9-d64cf4be8531?apiKey=4638d3c0f2964543b1679eba321e1d84&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/9add3222-3b43-491c-aca9-d64cf4be8531?apiKey=4638d3c0f2964543b1679eba321e1d84&" className="aspect-[0.78] object-contain object-center w-[42px] h-[54px] shrink-0 self-center" />
+          <div className="items-start flex max-w-full w-[627px] flex-row self-center justify-between gap-5 pr-2 py-1.5 max-md:flex-wrap max-md:justify-center">
+            <a href="..." className="text-lime-400 text-2xl mt-px"> Home </a>
+            <a href="..." className="text-zinc-50 text-2xl w-[137px] items-start -mt-px"> For Business </a>
+            <a href="..." className="text-zinc-50 text-2xl w-[142px] items-start -mt-px"> For Suppliers </a>
+            <a href="..." className="text-zinc-50 text-2xl w-[107px] items-start mt-px"> Use Cases </a>
+            <a href="..." className="text-zinc-50 text-2xl self-stretch items-start mt-px"> FAQ </a>
+          </div>
+        </section>
+        <section className="flex max-w-full w-[375px] flex-row items-start justify-between gap-5 max-md:justify-center">
+          <div className="items-start flex flex-row self-center gap-2 mt-1 max-md:justify-center">
+            <img loading="lazy" srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/0d4d9836-a6ca-453f-9894-011f22e7f149?apiKey=4638d3c0f2964543b1679eba321e1d84&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/0d4d9836-a6ca-453f-9894-011f22e7f149?apiKey=4638d3c0f2964543b1679eba321e1d84&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/0d4d9836-a6ca-453f-9894-011f22e7f149?apiKey=4638d3c0f2964543b1679eba321e1d84&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/0d4d9836-a6ca-453f-9894-011f22e7f149?apiKey=4638d3c0f2964543b1679eba321e1d84&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/0d4d9836-a6ca-453f-9894-011f22e7f149?apiKey=4638d3c0f2964543b1679eba321e1d84&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/0d4d9836-a6ca-453f-9894-011f22e7f149?apiKey=4638d3c0f2964543b1679eba321e1d84&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/0d4d9836-a6ca-453f-9894-011f22e7f149?apiKey=4638d3c0f2964543b1679eba321e1d84&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/0d4d9836-a6ca-453f-9894-011f22e7f149?apiKey=4638d3c0f2964543b1679eba321e1d84&" className="aspect-[1] object-contain object-center w-8 h-8 shrink-0 self-center" />
+            <a href="..." className="text-zinc-50 text-lg self-stretch"> EN </a>
+            <img loading="lazy" srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/0d41cc4e-5277-406c-a375-91ab4dc8d0b6?apiKey=4638d3c0f2964543b1679eba321e1d84&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/0d41cc4e-5277-406c-a375-91ab4dc8d0b6?apiKey=4638d3c0f2964543b1679eba321e1d84&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/0d41cc4e-5277-406c-a375-91ab4dc8d0b6?apiKey=4638d3c0f2964543b1679eba321e1d84&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/0d41cc4e-5277-406c-a375-91ab4dc8d0b6?apiKey=4638d3c0f2964543b1679eba321e1d84&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/0d41cc4e-5277-406c-a375-91ab4dc8d0b6?apiKey=4638d3c0f2964543b1679eba321e1d84&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/0d41cc4e-5277-406c-a375-91ab4dc8d0b6?apiKey=4638d3c0f2964543b1679eba321e1d84&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/0d41cc4e-5277-406c-a375-91ab4dc8d0b6?apiKey=4638d3c0f2964543b1679eba321e1d84&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/0d41cc4e-5277-406c-a375-91ab4dc8d0b6?apiKey=4638d3c0f2964543b1679eba321e1d84&" className="aspect-[1] object-contain object-center w-6 h-6 shrink-0 self-center" />
+          </div>
+          <div className="items-start flex flex-col self-center rounded-2xl">
+            <form className="justify-center items-center border border-lime-400 bg-stone-900 flex flex-col self-stretch pl-1 pt-1 pb-3.5 rounded-2xl border-solid">
+              <input type="text" className="bg-lime-400 z-[1] flex max-w-full w-[101px] min-h-[52px] flex-col self-center ml-0.5 rounded-2xl" />
+              <a href="..." className="text-zinc-50 text-base mt-0 self-center"> Sign In </a>
+            </form>
+          </div>
+          <div className="items-start flex flex-col self-stretch rounded-2xl">
+            <form className="justify-center items-center border-zinc-50 bg-stone-900 flex flex-col self-stretch pl-1.5 pt-1.5 pb-3.5 rounded-2xl border-2 border-solid">
+              <input type="text" className="border-zinc-50 bg-zinc-50 z-[1] flex max-w-full w-[105px] min-h-[50px] flex-col self-center ml-1 rounded-2xl border-2 border-solid" />
+              <a href="..." className="text-zinc-50 text-base mt-0 self-center"> Sign Up </a>
+            </form>
+          </div>
+        </section>
+      </div>
+    </section>
+  );
+}
